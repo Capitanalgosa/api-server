2aa6f9aa87ac93db43f44e610c316addb08a2f84
Capitanalgosa <ricardo.torrejas@gmail.com>
2020-06-08 15:28:54 -0400

    Funciona Perfectamente, siempre tiene que correr desde el puerto 4000, por que desde angular esta haciendo las llamadas al puerto 4000, para sacar la data

--- src/systemVariable.js
+++ src/systemVariable.js
@@ -1,14 +1,12 @@
 const systemVariable = {
     "URI_db": {
     "uri": "mongodb://",
-    "server": "localhost",
+    "server": "138.197.207.192",
     "name_db": 'tupirata',
     "full": function() {
         return this.uri+this.server+"/"+this.name_db
     }
 }
 };
-
-
 
 module.exports = systemVariable;
