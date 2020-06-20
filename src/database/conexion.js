b120a1acfef765814bf64433ee2156a570ef7793
Capitanalgosa <ricardo.torrejas@gmail.com>
2020-06-20 06:29:22 -0400

    Con base de datos autentificada

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -5,7 +5,7 @@ const system_variable = require('../systemVariable');
 
 mongoose.connect(system_variable.URI_db.full())
     .then(db => console.log(chalk.green(`Estas conectado a la base de datos ${chalk.bold.white(system_variable.URI_db.name_db)}`)))
-    .catch(err => {console.log(chalk.red('Ah ocurrido un error no se ah podido conectar a la base de datos'))
+    .catch(err => {console.log(chalk.red('Ah ocurrido un error no se ah podido conectar a la base de datos')+"\n"+ err)
 }    
 );
     
--- src/systemVariable.js
+++ src/systemVariable.js
@@ -1,10 +1,14 @@
 const systemVariable = {
     "URI_db": {
     "uri": "mongodb://",
-    "server": "138.197.207.192",
+    "server": "67.205.176.78",
+    "puerto": "28120",
+    "pene": "xdEop233fgt",
+    "vagina": "hhhfre456cds",
+    "culo": "admin",
     "name_db": 'tupirata',
     "full": function() {
-        return this.uri+this.server+"/"+this.name_db
+        return this.uri+this.pene+':'+this.vagina+'@'+this.server+':'+this.puerto+"/"+this.name_db+'?authSource='+this.culo
     }
 }
 };
