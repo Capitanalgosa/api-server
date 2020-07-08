90be1f360dfc48297fe238f683a5c8323b3778be
Capitanalgosa <ricardo.torrejas@gmail.com>
2020-07-08 17:08:20 -0400

    cambios

--- src/routes/render.js
+++ src/routes/render.js
@@ -4,7 +4,7 @@ const render= require('../controllers/render');
 
 router.get('/', render.goHome);
 router.get('/home', render.cient);
-router.get('/home/*', render.notFound);
+router.get('/home/*', render.notFound); 
 router.get('/admin', render.admin);
 router.get('/admin/*', render.notFound);
 router.get('/*', render.notFound);
